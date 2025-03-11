1bcc4aeb0ac5b7d511204fd5b968835b5f71aa80
kanchan0 <coolkanchan13@gmail.com>
2025-03-11 17:27:10 +0530

    updated some features

--- train_sweep.py
+++ train_sweep.py
@@ -6,73 +6,7 @@ from NeuralNetwork import NN
 from keras.datasets import fashion_mnist
 from Propagations import propagations
 from Optimizor4BackProp import optimization
-
-
-sweep_configs = {
-
-            "name": "DL-Assignment-1_test",
-            "metric": {
-                        "name":"Validation Accuracy",
-                        "goal": "maximize"
-                      },
-            "method": "bayes",
-            "early_terminate": {
-                        "type": "hyperband",
-                        "min_iter": 2,
-                        "eta" : 2
-                      },
-            "parameters": {
-                    "hidden_size": {
-                        "values": [32, 64, 128]
-                    },
-                    "num_layers": {
-                        "values": [3,4, 5]
-                    },
-                    "activation": {
-                        "values": ["sigmoid", "tanh", "ReLU"]
-                    },
-                    "optimizer": {
-                        "values": ["sgd", "momentum", "nag", "rmsprop", "adam", "nadam"]
-                    },
-                    "epochs": {
-                        "values": [5,15 ,10, 20]
-                    },
-                    "batch_size": {
-                        "values": [8, 16, 64, 128]
-                    },
-                    "learning_rate": {
-                        "values": [0.001, 0.0001,0.01]
-                    },
-                    "weight_init": {
-                        "values": ["random", "Xavier"]
-                    },
-                    "weight_decay": {
-                        "values": [0.005, 0.05]
-                    },
-                    "loss": {
-                        #"values": ["cross_entropy"]
-                        "values" :["mean_squared_error"]
-                    },
-                    "output_activation": {
-                        "values": ["softmax"]
-                    },
-                    "momentum": {
-                        "values": [0.9]
-                    },
-                    "beta": {
-                        "values": [0.9]
-                    },
-                    "beta1": {
-                        "values": [0.9]
-                    },
-                    "beta2": {
-                        "values": [0.999]
-                    },
-                    "epsilon": {
-                        "values": [1e-8]
-                    }
-            }
-            }
+from CONFIG import sweep_configs
 
 hyperparameter_defaults = dict(
     batch_size = 64,
@@ -89,7 +23,7 @@ def train():
     #Initialise wandb
     wandb.init(config=hyperparameter_defaults, 
                project="CS23S025-Assignment-1-DA6401-DL",
-                 entity="cs23s025-indian-institute-of-technology-madras")#, name=sweep_name)
+               entity="cs23s025-indian-institute-of-technology-madras")#, name=sweep_name)
     
     wandb_configs = wandb.config
     sweep_name = wbhelp.sweep_name(wandb_configs)
